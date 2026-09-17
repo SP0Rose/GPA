62958381092fb2a1919afbe08d243f63e9a46db1
619551 <pipes.619551@swl.k12.oh.us>
2026-09-17 11:54:14 -0400

    commit

--- src/GPA.java
+++ src/GPA.java
@@ -4,28 +4,39 @@ public class GPA {
     static void main(String[] args) {
         // Variables
         Scanner scan = new Scanner(System.in);
-        int age;
-        double gpa;
-        String name;
+        int age = 0;
+        double gpa = 0;
+        String name = "";
 
         // Outputs
         System.out.println("What's your name?");
         name = scan.nextLine();
 
         System.out.println("How old are you?");
-        age = scan.nextInt();
-        if (age < 0 || age > 18){
+        if (scan.hasNextInt()) {
+            age = scan.nextInt();
+            if (age < 0 || age > 19){
+                System.out.println("That is not a valid input.");
+                System.exit(0);
+            }
+        } else {
             System.out.println("That is not a valid input.");
             System.exit(0);
         }
 
         System.out.println("What's your GPA?");
-        gpa = scan.nextDouble();
-        if (gpa < 0 || gpa > 4){
+        if (scan.hasNextDouble()) {
+            gpa = scan.nextDouble();
+            if (gpa < 0 || gpa > 5){
+                System.out.println("That is not a valid input.");
+                System.exit(0);
+            }
+        } else {
             System.out.println("That is not a valid input.");
             System.exit(0);
         }
 
+        // Output message
         System.out.println("Hello, " + name + ". You are " + age + " years old and have a GPA of " + gpa + ".");
     }
 }
